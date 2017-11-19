c8ab6aea0d0abccd6d89d2008b730970490b6cef
Inês Jorge <icjea@iscte-iul.pt>
2017-11-19 20:57:51 +0000

    Ler ficheiros spam.log e ham.log

--- src/antiSpamFilter/FileReader.java
+++ src/antiSpamFilter/FileReader.java
@@ -8,12 +8,21 @@ import java.util.Scanner;
 public class FileReader {
 
 	private File fileRules;
+	private File fileSpam;
+	private File fileHam;
 	private ArrayList<Rule> rules;
-
+	private String ruleEmail;
+	
 	public FileReader() {
 		fileRules = new File("rules.cf");
 		rules = new ArrayList<Rule>();
 		readFileRules(fileRules);
+		
+		fileSpam = new File("spam.log.txt");
+		readFileSpamHam(fileSpam);
+		
+		fileHam = new File("ham.log.txt");
+		readFileSpamHam(fileHam);
 	}
 
 	public void readFileRules(File f) {
@@ -35,4 +44,33 @@ public class FileReader {
 	public ArrayList<Rule> getRules() {
 		return rules;
 	}
+	
+	
+	public void readFileSpamHam(File filename) {
+		ArrayList<String> rulesEmail = new ArrayList<>();
+		
+		try {
+			Scanner sc = new Scanner(filename);
+			
+			while(sc.hasNextLine()) {
+				String line = sc.nextLine();
+				String[] division = line.split("	");
+				String emailId = division[1];
+			
+				for(int i = 1; i < division.length; i++ ) {
+					ruleEmail = division[i];
+					
+					//confirmar se le as regras
+					rulesEmail.add(ruleEmail);
+				}
+				
+			}
+		}catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.out.println("rulesEmail: " + rulesEmail);
+	}
+	
+	
+	
 }
