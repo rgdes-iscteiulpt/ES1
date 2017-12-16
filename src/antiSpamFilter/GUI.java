08b8125d40ae0b0302dda1a70757129b61a9b90a
hgpml-iscteiulpt <hgpml@iscte-iul.pt>
2017-12-16 14:25:54 +0000

    JavaDoc - Comentários na Class GUI

--- src/antiSpamFilter/GUI.java
+++ src/antiSpamFilter/GUI.java
@@ -18,6 +18,14 @@ import javax.swing.ScrollPaneConstants;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
+/**
+ * Classe da Interface Gráfica do Utilizador: Cria Frame e objectos associados à
+ * mesma Introduz valores nas tabelas Rgras/Pesos
+ */
+/**
+ * @author hogum
+ *
+ */
 public class GUI {
 
 	public JFrame frame = new JFrame("Anti-Spam Configuration For Professional Mail-Box");
@@ -26,7 +34,7 @@ public class GUI {
 	private JPanel scrollPanel = new JPanel();
 	private JPanel scrollPanel2 = new JPanel();
 	private JPanel b1 = new JPanel();
-	private JPanel b2 =  new JPanel();;
+	private JPanel b2 = new JPanel();;
 	private JPanel buttonPanel1 = new JPanel();
 	private JPanel buttonPanel2 = new JPanel();
 	private JPanel fpfnPanel = new JPanel();
@@ -39,9 +47,9 @@ public class GUI {
 	private JTextField textspam = new JTextField();
 	private JTextField textham = new JTextField();
 	public JLabel l1 = new JLabel("         Escolher Path:      ");
-	public JLabel l2 = new JLabel("		rules.cf		"); //rules.cf
-	public JLabel l3 = new JLabel("		spam.log		"); //spam.log.txt
-	public JLabel l4 = new JLabel("		ham.log			"); //ham.log.txt
+	public JLabel l2 = new JLabel("		rules.cf		"); // rules.cf
+	public JLabel l3 = new JLabel("		spam.log		"); // spam.log.txt
+	public JLabel l4 = new JLabel("		ham.log			"); // ham.log.txt
 	private JPanel manual = new JPanel();
 	private JPanel auto = new JPanel();
 	public JLabel m = new JLabel("Configuração Manual");
@@ -62,30 +70,38 @@ public class GUI {
 	private JPanel path = new JPanel();
 	private JPanel esquerda = new JPanel();
 	private JPanel direita = new JPanel();
-	private boolean updateBoolean =false;
-	private boolean resultBoolean =false;
-	private boolean result2Boolean =false;
-	private boolean saveBoolean =false;
-	private boolean save2Boolean =false;
-	
+	private boolean updateBoolean = false;
+	private boolean resultBoolean = false;
+	private boolean result2Boolean = false;
+	private boolean saveBoolean = false;
+	private boolean save2Boolean = false;
+
 	private FileManager r;
 	private String[][] data;
 	private String[][] data2;
-	
+
+	/**
+	 * Default Constructor GUI
+	 */
+
 	public GUI() {
 		init();
 	}
 
-	// Cria a Janela de Regras/Pesos
+	/**
+	 * Método que cria frame inicial, onde se escolhem os ficheiros (Rules, Ham,
+	 * Spam)
+	 */
+
 	public void init() {
 		frame = new JFrame("Anti-Spam Configuration For Professional Mail-Box");
 		frame.setLayout(new BorderLayout());
 
 		path.setLayout(new BorderLayout());
-		
+
 		esquerda.setLayout(new BorderLayout());
 		esquerda.add(l1);
-		
+
 		direita.setLayout(new GridLayout(6, 1));
 
 		direita.add(l2);
@@ -94,16 +110,17 @@ public class GUI {
 		direita.add(textspam);
 		direita.add(l4);
 		direita.add(textham);
-		
+
 		update.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				contentUpdateButton();
-				updateBoolean=true;
+				updateBoolean = true;
 			}
 		});
 
-		path.add(update, BorderLayout.SOUTH); //só depois de darmos path para rules é que podes ler as rules e fazer o resto da gui
+		path.add(update, BorderLayout.SOUTH); // só depois de darmos path para rules é que podes ler as rules e fazer o
+												// resto da gui
 		path.add(esquerda, BorderLayout.WEST);
 		path.add(direita, BorderLayout.EAST);
 		frame.add(path, BorderLayout.NORTH);
@@ -113,18 +130,24 @@ public class GUI {
 		frame.setVisible(true);
 	}
 
+	/**
+	 * Método que desenha a frame principal, posteriormente a escolher a path, que
+	 * irá conter: as tabelas Regras/Pesos para cada tipo de configuração
+	 * (Automática/Manual), o número de falsos positivos e falsos negativos e botões
+	 * que permitem guardar alterações ou executar testes;
+	 */
 	public void updateGUI() {
-		//1 painel para cada tipo de configuração
+		// Um painel para cada tipo de configuração
 		manual.setLayout(new BorderLayout());
 		auto.setLayout(new BorderLayout());
-									
+
 		manual.add(m, BorderLayout.NORTH);
 		auto.add(a, BorderLayout.NORTH);
 
-		// Acrescentar o scroll à tabela	
+		// Acrescentar o scroll à tabela
 		scrollPanel.setBorder(new TitledBorder(new EtchedBorder(), "Rules List"));
 		scrollPanel2.setBorder(new TitledBorder(new EtchedBorder(), "Rules List"));
-		
+
 		JScrollPane scroll = new JScrollPane(table);
 		JScrollPane scroll2 = new JScrollPane(table1);
 
@@ -136,16 +159,16 @@ public class GUI {
 
 		manual.add(scrollPanel, BorderLayout.CENTER);
 		auto.add(scrollPanel2, BorderLayout.CENTER);
-		
-		// Acrescentar ao painel de botões os botões;	
+
+		// Acrescentar ao painel de botões os botões;
 		b1.setLayout(new BorderLayout());
 		b2.setLayout(new BorderLayout());
-		
+
 		buttonPanel1.setLayout(new GridLayout(1, 2));
 		buttonPanel2.setLayout(new GridLayout(1, 2));
-		
+
 		addButtons();
-		
+
 		// Painel dos falsos positivos e falsos negativos
 		addFPanel();
 
@@ -153,13 +176,13 @@ public class GUI {
 		fpfnPanel.add(numberOfFalsePositives);
 		fpfnPanel.add(falseNegatives);
 		fpfnPanel.add(numberOfFalseNegatives);
-		
+
 		fpfnPanel2.add(falsePositives2);
 		fpfnPanel2.add(numberOfFalsePositives2);
 		fpfnPanel2.add(falseNegatives2);
 		fpfnPanel2.add(numberOfFalseNegatives2);
-		
-		//Adicionais todos os botoes ao painel dos botoes
+
+		// Adicionais todos os botoes ao painel dos botoes
 		b1.add(buttonPanel1, BorderLayout.NORTH);
 		b1.add(fpfnPanel, BorderLayout.SOUTH);
 
@@ -177,8 +200,16 @@ public class GUI {
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
-	
-	// Cria a tabela correspondente a Janela
+
+	/**
+	 * Método que cria as tabela Regras/Pesos para cada tipo de
+	 * configuração(Manual/Automática); Introduz as regras que constam no ArrayList
+	 * rules nas tabelas;
+	 * 
+	 * @param rules
+	 *            corresponde ao Arraylist de Regras
+	 * 
+	 */
 
 	public void createTable(ArrayList<Rule> rules) {
 		String[] columns = { "Rules", "Weights" };
@@ -211,157 +242,188 @@ public class GUI {
 		};
 	}
 
-	//Executado no botao update - cria tabela com as regrs do ficheiro rules.cf 
+	/**
+	 * Método invocado no método Init() quando pressionado o botão update ("Get
+	 * Rules"); Cria um FileManager que lê o ficheiro de regras correspondente à
+	 * path introduzida; Invoca o método createTable() e posteriormente o
+	 * updateGUI();
+	 * 
+	 */
+
 	public void contentUpdateButton() {
 		r = new FileManager(textrules.getText());
 		createTable(r.getRules());
 		updateGUI();
 	}
-	
-	//Executado no botao result
+
+	/**
+	 * Método executado quando pressionado o botão resultButton correspondente à
+	 * configuração Manual; Consiste em inserir os pesos correspondentes às regras
+	 * da tabela, ao ArrayList rules (rules.getRule().setWeight()); Lê os ficheiros
+	 * Ham e spam e introduz o número de Falsos positivos e negativos;
+	 */
+
 	public void contentResultButton() {
 		// A cada peso introduzido, introduz no arraylist de regras o
 		// peso correspondente a cada regra
 		int weight = 0;
 		for (int i = 0; i < r.getRules().size(); i++) {
 			String s = (String) table.getValueAt(i, 1);
-			
+
 			if (s == null) {
 				s = "0";
 			}
 			weight = Integer.parseInt(s);
 			r.getRules().get(i).setWeight(weight);
-		
+
 		}
 
-		//Ler ficheiros Spam.log e Ham.log
+		// Ler ficheiros Spam.log e Ham.log
 		r.readFileSpam(textspam.getText());
 		r.readFileHam(textham.getText());
-	
+
 		// Introduzir o número de falsos positivos e falsos negativos na janela
 		p = Double.toString(r.getNumberOfFalsePositives());
 		numberOfFalsePositives.setText(p);
 		n = Double.toString(r.getNumberOfFalseNegatives());
 		numberOfFalseNegatives.setText(n);
 	}
-	
-	//Executado no botao result2
+
+	/**
+	 * Método executado quando pressionado o botão resultButton correspondente à
+	 * configuração Automática; Consiste em gerar automáticamente os pesos
+	 * respectivos a cada regra, através de um algoritmo, e adiciona-os na tabela;
+	 * Lê os ficheiros que contêm os pesos e o número de falsos positivos,
+	 * calculados pelo algoritmo e introduz o esses valores na tabela;
+	 */
+
 	public void contentResultButton2() {
 		AntiSpamFilterAutomaticConfiguration a = new AntiSpamFilterAutomaticConfiguration();
 		try {
-			//Gerar configuração automática
+			// Gerar configuração automática
 			a.automaticConfiguration(textrules.getText(), textspam.getText(), textham.getText());
-			
-			//Ler ficheiros gerados pela configuração automática
+
+			// Ler ficheiros gerados pela configuração automática
 			r.readRf();
 			r.readRs();
-			
-			//Introduzir configuração dos pesos na janela
-			for (int i = 0; i <r.getRules().size(); i++) {
-				data2[i][1]=Double.toString(r.getRules().get(i).getWeight());
+
+			// Introduzir configuração dos pesos na janela
+			for (int i = 0; i < r.getRules().size(); i++) {
+				data2[i][1] = Double.toString(r.getRules().get(i).getWeight());
 			}
-			
+
 			// Introduzir o número de falsos positivos e falsos negativos na janela
 			pa = Double.toString(r.getNumberOfFalsePositives());
 			numberOfFalsePositives2.setText(pa);
-			
+
 			na = Double.toString(r.getNumberOfFalseNegatives());
 			numberOfFalseNegatives2.setText(na);
-			
+
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
-		
+
 	}
-	
-	
+
+	/**
+	 * Método que adiciona ActionListeners aos botões saveButton e resultButton para
+	 * cada tipo de configuração (Automática/Manual);
+	 */
 	public void addButtons() {
-		// Ao selecionaro Botao "Save changes" guardamos o resultado dos pesos que demos às regras ao ficheiros rules.cf	
+		// Ao selecionaro Botao "Save changes" guardamos o resultado dos pesos que demos
+		// às regras ao ficheiros rules.cf
 		saveButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				r.writeRulesFile();
-				saveBoolean=true;
+				saveBoolean = true;
 				reset();
 			}
 		});
-		
-		
+
 		saveButton2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				r.writeRulesFile();	
-				save2Boolean=true;
+				r.writeRulesFile();
+				save2Boolean = true;
 				reset();
 			}
 		});
-		
+
 		resultButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				contentResultButton();
-				resultBoolean=true;
+				resultBoolean = true;
 				reset();
 			}
 		});
-		
+
 		resultButton2.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				contentResultButton2();
-				result2Boolean=true;
+				result2Boolean = true;
 				reset();
 			}
 		});
 
 		buttonPanel1.add(resultButton);
 		buttonPanel1.add(saveButton);
-			
+
 		buttonPanel2.add(resultButton2);
 		buttonPanel2.add(saveButton2);
 	}
-	
+
+	/**
+	 * Método que adiciona os Paineis de Falsos positivos e negativos,
+	 * correspondentes a cada tipo de configuração (Automática/Manual), à frame;
+	 */
 	public void addFPanel() {
-		fpfnPanel.setLayout(new GridLayout(2,2));
-		fpfnPanel2.setLayout(new GridLayout(2,2));
-		
+		fpfnPanel.setLayout(new GridLayout(2, 2));
+		fpfnPanel2.setLayout(new GridLayout(2, 2));
+
 		falsePositives.setHorizontalAlignment(JLabel.CENTER);
 		numberOfFalsePositives.setEditable(false);
-		
+
 		falsePositives2.setHorizontalAlignment(JLabel.CENTER);
 		numberOfFalsePositives2.setEditable(false);
 
 		falseNegatives.setHorizontalAlignment(JLabel.CENTER);
 		numberOfFalseNegatives.setEditable(false);
-		
+
 		falseNegatives2.setHorizontalAlignment(JLabel.CENTER);
 		numberOfFalseNegatives2.setEditable(false);
 	}
-	
-	
+
+	/**
+	 * Método que verifica se todos os botões foram accionados, e em caso
+	 * afirmativo, elemina todos os valores contidos nos Arraylists e contidos na
+	 * tabela; Limpa os valores do sistema após utilização total;
+	 */
 	public void reset() {
-		if(updateBoolean == true && resultBoolean==true && result2Boolean==true && save2Boolean==true && saveBoolean==true) {
-			 r.getRules().clear();
-			 r.fPositives.clear();
-			 r.fNegatives.clear();
-			 r.problemList.clear();
-			 updateBoolean =false;
-			 resultBoolean =false;
-			 result2Boolean =false;
-			 saveBoolean =false;
-			 save2Boolean =false;
-			 
-			 for(int i =0; i< data.length; i++) {
-				 data[i][0]=null;
-				 data[i][1]=null;
-			 }
-				 
-			 for(int i =0; i< data2.length; i++) { 
-				 data2[i][0]=null;
-				 data2[i][1]=null;
-			 }
+		if (updateBoolean == true && resultBoolean == true && result2Boolean == true && save2Boolean == true
+				&& saveBoolean == true) {
+			r.getRules().clear();
+			r.fPositives.clear();
+			r.fNegatives.clear();
+			r.problemList.clear();
+			updateBoolean = false;
+			resultBoolean = false;
+			result2Boolean = false;
+			saveBoolean = false;
+			save2Boolean = false;
+
+			for (int i = 0; i < data.length; i++) {
+				data[i][0] = null;
+				data[i][1] = null;
+			}
+
+			for (int i = 0; i < data2.length; i++) {
+				data2[i][0] = null;
+				data2[i][1] = null;
+			}
 		}
 	}
 
-	
 }
